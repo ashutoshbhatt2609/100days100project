43873f38d83f76fdf4e791b98a7251a145d228c2
Ashutoshbhatt2609 <ashutoshbhatt2609@gamil.com>
2024-08-29 12:22:15 +0530

    update

--- #1.py
+++ #1.py
@@ -1,4 +1,4 @@
 print("Welcome to band name generator!")
-a=input("What is the name of your city?")
-b=input("What is the name of your pet?")
+a=input("What is the name of your city?\n")
+b=input("What is the name of your pet?\n")
 print("Your band name could be", a,b)
