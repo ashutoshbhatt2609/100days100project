2e2c7538c521bc89cc8db2934bb86d0b4cf5b49b
Ashutoshbhatt2609 <ashutoshbhatt2609@gamil.com>
2024-08-29 12:41:08 +0530

    update

--- #2.py
+++ #2.py
@@ -1,5 +1,5 @@
 print("Welcome to the tip calculator!")
-amnt=int(input("What was the toatl bill? \n$"))
+amnt=float(input("What was the toatl bill? \n$"))
 ppl=int(input("How many people to split the bill?\n"))
 tip=int(input("How much percentage of tip you want to give? 10,12 or 15?\n"))
 total=amnt +amnt*(tip/100)
