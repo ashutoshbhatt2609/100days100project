c1cf553a75fd095917995c484492beaf739f309f
Ashutoshbhatt2609 <ashutoshbhatt2609@gamil.com>
2024-08-29 12:35:36 +0530

    update

---
+++ #2.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator!")
+amnt=int(input("What was the toatl bill? \n$"))
+ppl=int(input("How many people to split the bill?\n"))
+tip=int(input("How much percentage of tip you want to give? 10,12 or 15?\n"))
+total=amnt +amnt*(tip/100)
+each_pay=total/ppl
+print("Each hss to pay $",each_pay)
