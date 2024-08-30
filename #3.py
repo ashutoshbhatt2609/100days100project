cc1aa9bd0f5efd066f1528337e352640025aa9c5
Ashutoshbhatt2609 <ashutoshbhatt2609@gamil.com>
2024-08-30 13:21:01 +0530

    update

--- #3.py
+++ #3.py
@@ -1,6 +1,6 @@
 #Love calculator
-a1=input("Enter your name 1:-").lower()
-a2=input("Enter your name 2:-").lower()
+a1=input("Enter your name 1:-\n").lower()
+a2=input("Enter your name 2:-\n").lower()
 s=str(a1)+str(a2)
 t,r,u,e,l,o,v,e=s.count("t"),s.count("r"),s.count("u"),s.count("e"),s.count("l"),s.count("o"),s.count("v"),s.count("e")
 perc=(t+r+u+e)*10+(l+o+v+e)
