225a880ae8dd80dd63a8e10147b1cabb0ba7d1a0
Ashutoshbhatt2609 <ashutoshbhatt2609@gamil.com>
2024-09-06 13:39:03 +0530

    update

---
+++ hangman/caesar cipher/main.py
@@ -0,0 +1,39 @@
+from art import *
+print(logo)
+msg=input("Enter your message:-\n").lower()
+shift=int(input("Type the shift number:-"))
+direction=(input("Enter wether to Decode or Encode:-"))
+
+def encode(plain_text,shift_amount):
+    text=plain_text.split(" ")
+    ans=""
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for words in text:
+        cipher=""
+        for j in words:
+            index2=alphabet.index(j)
+            cipher+=alphabet[index2+shift]
+        ans+=cipher
+        ans+=" "
+    print("The encoded text is:-",ans)
+
+def decode(plain_text,shift_amount):
+    text=plain_text.split(" ")
+    ans=""
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for words in text:
+        cipher=""
+        for j in words:
+            index2=alphabet.index(j)
+            cipher+=alphabet[index2-shift]
+        ans+=cipher
+        ans+=" "
+    print("The decoded text is:-",ans)
+        
+def caesar_cipher(shift_amount,direction_given,msg_typed):
+    if direction=="Encode":
+        encode(msg_typed,shift_amount)
+    elif direction=="Decode":
+        decode(msg_typed,shift_amount)
+            
+caesar_cipher(shift,direction,msg)
