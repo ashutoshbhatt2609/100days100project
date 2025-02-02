88d1865f466f6d78f0cb15cf000bb94086339057
Ashutoshbhatt2609 <ashutoshbhatt2609@gamil.com>
2025-02-02 14:26:59 +0530

    Update

--- DAy-25/main.py
+++ DAy-25/main.py
@@ -1,5 +1,21 @@
 import csv
-with open("DAy-25\weather_data.csv,'r") as data_file:
-    data=csv.reader(data_file)
+with open("DAy-25\weather_data.csv",'r') as data_file:
+    data= csv.reader(data_file)
+    date=[]
+    temp=[]
+    weather=[]
     for row in data:
-        print(row)
+        if row[0]!='day':
+            date.append(row[0])
+        if row[1]!='temp':
+            temp.append(int(row[1]))
+        if row[0]!='condition':
+            weather.append(row[2])
+
+
+import pandas
+data2=pandas.read_csv("DAy-25\weather_data.csv")
+print(data2)
+print(data2['temp'])
+print(data2['day'])
+print(data2['condition'])
